82455c43495e90d712b51fb40e92f3196ae99c27
ahmad najjar <Ahmad.najjarr93@gmail.com>
2019-02-04 19:50:42 +0200

    ..

--- src/main/java/com/example/DataBase/web/ViewDefectController.java
+++ src/main/java/com/example/DataBase/web/ViewDefectController.java
@@ -2,30 +2,42 @@ package com.example.DataBase.web;
 
 import java.util.ArrayList;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.felix.bundlerepository.Repository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.example.DataBase.Repository.DefectInstanceRepository;
-import com.example.DataBase.Repository.ViewDefectsRepository;
 import com.example.DataBase.domain.ViewDefects;
 import com.example.DataBase.domain.ViewDefectsApp;
 
 @RestController
 public class ViewDefectController {
 	@Autowired
-	private ViewDefectsRepository repository;
-	
-	@Autowired
+	private DefectInstanceRepository repository;
+
+@Autowired
 	private DefectInstanceRepository repository1;
 
-	@RequestMapping("/ViewDefects")
-	public ArrayList<ViewDefects> getViewDefects() {
-		return repository.getViewDefects();
+	
+	
+	@RequestMapping("/ViewDefects/pages/{pageno}")
+	
+	
+	@ResponseBody 
+    public ArrayList<ViewDefects> getViewDefects(@PathVariable("pageno") int pageno,
+    		HttpServletRequest req, HttpServletResponse res) throws ServletException {
+		
+		return repository.getViewDefects(new PageRequest(pageno,10));
 		
 	}
-	
+
 	@RequestMapping("/ViewDefectsApp/{appName}")
 	public ArrayList<ViewDefectsApp> getViewDefectsApp(@PathVariable String appName) {
 		return repository1.getViewDefectsApp(appName);
